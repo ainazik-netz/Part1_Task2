bffaead37523ebd063c90287217725ff411fbda2
ainazik-netz <ainazik0807@gmail.com>
2019-10-24 12:22:51 +0600

    first commit

---
+++ task2.py
@@ -0,0 +1,8 @@
+def square_number(num):
+    return num*num
+def test_square_number():
+    assert square_number(3) == 9
+    assert square_number(4) == 16
+    assert square_number(5) == 25
+print('Your code is correct!')
+test_square_number()
